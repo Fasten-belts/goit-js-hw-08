84a30a72c7e437177912d87f59fc27e5adfa4114
Fasten-belts <124335932+Fasten-belts@users.noreply.github.com>
2023-06-30 14:01:37 +0200

    homework 2

---
+++ src/js/02-video.js
@@ -0,0 +1,25 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const initialTime = localStorage.getItem('videoplayer-current-time');
+// console.log(typeof initialTime, initialTime);
+
+function onTimeUpdate(data) {
+  const currentTime = data.seconds;
+
+  localStorage.setItem('videoplayer-current-time', currentTime.toString());
+}
+
+function initializePlayer() {
+  if (initialTime) {
+    player.setCurrentTime(parseFloat(initialTime));
+  }
+  // console.log(typeof parseFloat(initialTime), parseFloat(initialTime));
+
+  player.on('timeupdate', throttle(onTimeUpdate, 1000));
+}
+
+initializePlayer();
