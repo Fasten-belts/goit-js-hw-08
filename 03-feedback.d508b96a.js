6817e49a6c98d14d5155fb88779987a767c4dc40
Fasten-belts <124335932+Fasten-belts@users.noreply.github.com>
2023-07-02 20:05:46 +0000

    Deploying to gh-pages from @ Fasten-belts/goit-js-hw-08@21a4800c5f3827196d422d0477d739c911055819 🚀

--- 03-feedback.d508b96a.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{};var t="object"==typeof e&&e&&e.Object===Object&&e,o="object"==typeof self&&self&&self.Object===Object&&self;t||o||Function("return this")(),Object.prototype.toString}();
-//# sourceMappingURL=03-feedback.d508b96a.js.map
